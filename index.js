b58b63eaf09eedc84387ec8d7df07ed873574560
HRsilasDK1 <132482268+HRsilasDK1@users.noreply.github.com>
2026-07-22 23:25:53 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const button = document.getElementById("random-btn");
+const categoryBox = document.getElementById("category-box");
+
+let categories = [];
+
+async function loadCategories() {
+    try {
+        const response = await fetch("categories.json");
+        const data = await response.json();
+
+        categories = data.categories;
+    } catch (error) {
+        categoryBox.textContent = "Kunne ikke indlæse categories.json";
+        console.error(error);
+    }
+}
+
+button.addEventListener("click", () => {
+    if (categories.length === 0) return;
+
+    const randomIndex = Math.floor(Math.random() * categories.length);
+    categoryBox.textContent = categories[randomIndex];
+});
+
+loadCategories();
