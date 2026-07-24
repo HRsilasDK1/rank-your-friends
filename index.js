1005fe4b02e3115f02c417254d4e4d85803cd02e
HRsilasDK1 <132482268+HRsilasDK1@users.noreply.github.com>
2026-07-24 20:29:24 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ button.addEventListener("click", () => {
     if (categories.length === 0) return;
 
     const randomIndex = Math.floor(Math.random() * categories.length);
+    console.log(`Random index: ${randomIndex}, Category: ${categories[randomIndex]}, Categories length: ${categories.length}`);
     categoryBox.textContent = categories[randomIndex];
 });
 
